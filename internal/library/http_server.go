133326acf85f67f8bdc3faabe2a20c624a9c241b
kostyaBro <indianor201203@gmail.com>
2022-12-31 18:26:45 +0500

    feat: draft http server

---
+++ internal/library/http_server.go
@@ -0,0 +1,105 @@
+package library
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"go.uber.org/zap"
+
+	"github.com/MikhailKatarzhin/Library/internal/library/book/info"
+	"github.com/MikhailKatarzhin/Library/pkg/logger"
+)
+
+const DefaultTimeout = 30 * time.Second
+
+type HTTPServerOption func(*HTTPServer)
+
+func WithTimeout(timeout time.Duration) HTTPServerOption {
+	return func(server *HTTPServer) {
+		server.serv.ReadTimeout = timeout
+		server.serv.WriteTimeout = timeout
+	}
+}
+
+type HTTPServer struct {
+	serv           *http.Server
+	libraryService *Service
+}
+
+func NewHTTPServer(
+	port int, libraryService *Service, options ...HTTPServerOption,
+) *HTTPServer {
+	result := &HTTPServer{
+		serv: &http.Server{
+			Addr:         fmt.Sprintf(":%d", port),
+			ReadTimeout:  DefaultTimeout,
+			WriteTimeout: DefaultTimeout,
+		},
+		libraryService: libraryService,
+	}
+
+	for _, option := range options {
+		option(result)
+	}
+
+	result.registerHandlers()
+
+	return result
+}
+
+func (s *HTTPServer) Start() error {
+	return s.serv.ListenAndServe() //nolint:wrapcheck
+}
+
+func (s *HTTPServer) Close() error {
+	return s.serv.Close() //nolint:wrapcheck
+}
+
+func (s *HTTPServer) registerHandlers() {
+	http.HandleFunc("/book", s.handleBook)
+}
+
+func (s *HTTPServer) handleBook(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		s.handleBookPost(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+type BookJSON struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+func (s *HTTPServer) handleBookPost(w http.ResponseWriter, r *http.Request) {
+	book := new(BookJSON)
+	if err := json.NewDecoder(r.Body).Decode(book); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+
+		response := fmt.Sprintf("failed to decode book: %v", err)
+		if _, err = w.Write([]byte(response)); err != nil {
+			logger.I().Error(
+				"failed to write response",
+				zap.Error(err), zap.String("response", response),
+			)
+		}
+
+		return
+	}
+
+	if err := s.libraryService.AddNewBook(r.Context(), info.BookInfo{
+		Title:       book.Title,
+		Description: book.Description,
+	}); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		logger.I().Error("failed to add new book", zap.Error(err))
+
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
